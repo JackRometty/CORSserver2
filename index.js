d7d5f5ac1ced811e175aebeccc68a306bca8b3f3
Jack Rometty <JackRometty@users.noreply.github.com>
2016-12-23 10:50:22 -0600

    only iterates count if request comes from side project url

--- index.js
+++ index.js
@@ -10,21 +10,24 @@ app.use(function(req, res, next) {
 var count = 0;
 
 app.get('/', function(req, res, next) {
-	console.log(" GET REQUEST ");
-	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	console.log(ip);
-	res.send("fuck yes");
+        console.log(" GET REQUEST ");
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log(ip);
+        res.send("fuck yes");
 });
 
 app.post('/', function(req, res, next) {
-	console.log(" POST REQUEST ");
-	count += 1;
-	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-	console.log(ip);
-	console.log(req.get('origin'));
-	res.send(String(count));
+        console.log(" POST REQUEST ");
+        var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log(ip);
+        console.log(req.get('origin'));
+        if (req.get('origin') == "https://jackrometty.github.io") {
+                count += 1;
+        }
+        res.send(String(count));
 });
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
+
